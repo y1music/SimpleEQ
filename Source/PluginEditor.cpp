ed2741282b1f515970b0830c0d5117963e5b8495
y1music <yannikschmidt@icloud.com>
2024-05-04 16:56:53 +0200

    Connected Custom GUI to EQ Parameters

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,11 +11,15 @@
 
 //==============================================================================
 SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p),peakFreqSliderAttachment(audioProcessor.apvts, "PD_freq", peakFreqSlider), peakQSliderAttachment(audioProcessor.apvts, "PD_q", peakQSlider), peakGainSliderAttachment(audioProcessor.apvts, "PD_gain", peakGainSlider), lcFreqSliderAttachment(audioProcessor.apvts, "LC_freq", lcFreqSlider), lcSlopeSliderAttachment(audioProcessor.apvts, "LC_slope", lcSlopeSlider), hcFreqSliderAttachment(audioProcessor.apvts, "HC_freq", hcFreqSlider), hcSlopeSliderAttachment(audioProcessor.apvts, "HC_slope", hcSlopeSlider)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    for (auto* comp : getComps()) {
+        addAndMakeVisible(comp);
+    }
+    
+    setSize (1280, 720);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
@@ -37,4 +41,30 @@ void SimpleEQAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    auto bounds = getLocalBounds();
+    auto responseArea = bounds.removeFromTop(bounds.getHeight()*0.66);
+    
+    auto lcArea = bounds.removeFromLeft(bounds.getWidth()*0.33);
+    auto hcArea = bounds.removeFromRight(bounds.getWidth()*0.5);
+    lcFreqSlider.setBounds(lcArea.removeFromTop(lcArea.getHeight()*0.5));
+    lcSlopeSlider.setBounds(lcArea);
+    hcFreqSlider.setBounds(hcArea.removeFromTop(hcArea.getHeight()*0.5));
+    hcSlopeSlider.setBounds(hcArea);
+    
+    peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight()*0.33));
+    peakQSlider.setBounds(bounds.removeFromTop(bounds.getHeight()*0.5));
+    peakGainSlider.setBounds(bounds);
+}
+
+std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps() {
+    return {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &peakQSlider,
+        &lcFreqSlider,
+        &lcSlopeSlider,
+        &hcFreqSlider,
+        &hcSlopeSlider
+    };
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,12 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider {
+    
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox) {
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +34,27 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+    
+    CustomRotarySlider peakFreqSlider,
+    peakQSlider,
+    peakGainSlider,
+    lcFreqSlider,
+    lcSlopeSlider,
+    hcFreqSlider,
+    hcSlopeSlider;
+    
+    using APVTS = juce::AudioProcessorValueTreeState;
+    using Attachment = APVTS::SliderAttachment;
+    
+    Attachment peakFreqSliderAttachment,
+    peakQSliderAttachment,
+    peakGainSliderAttachment,
+    lcFreqSliderAttachment,
+    lcSlopeSliderAttachment,
+    hcFreqSliderAttachment,
+    hcSlopeSliderAttachment;
+    
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
